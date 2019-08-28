a9b3cbdf70619bd8b061588b8c13e8b68ad66a83
yuenwong2sps <31495374+yuenwong2sps@users.noreply.github.com>
2019-08-28 07:54:36 -0400

    enable host run

--- app.py
+++ app.py
@@ -260,5 +260,5 @@ def dict_factory(cursor, row):
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
-    #app.run(host="0.0.0.0", port=80)
+    #app.run(debug = True)
+    app.run(host="0.0.0.0", port=80)
